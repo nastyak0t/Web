a02d05e25e94edb7e80bf159ddc6134770a72cc9
nastyak0t <64741006+nastyak0t@users.noreply.github.com>
2020-05-13 22:46:36 +0700

    Web2
    Вторая лабораторная

---
+++ Web2/Web2/Controllers/CalcController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Web2.Models;
+
+namespace Web2.Controllers
+{
+    public class CalcController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ManualParsingInSingleAction()
+        {
+            return View();
+        }
+
+        public IActionResult ManualParsingInSeparateActions()
+        {
+            return View();
+        }
+
+        public IActionResult ModelBindingParametrs()
+        {
+            return View();
+        }
+
+        public IActionResult ModelBindingSeparateModel()
+        {
+            return View();
+        }
+
+        private double Calc(double value1, double value2, string operation)
+        {
+            double result;
+
+            switch (operation)
+            {
+                case "+":
+                    result = value1 + value2;
+                    break;
+
+                case "-":
+                    result = value1 - value2;
+                    break;
+
+                case "*":
+                    result = value1 * value2;
+                    break;
+
+                case "/":
+                    result = value1 / value2;
+                    break;
+
+                default:
+                    result = 0;
+                    break;
+            }
+
+            return result;
+        }
+
+        public IActionResult Count1()
+        {
+            if (Request.Method.Equals("POST", StringComparison.OrdinalIgnoreCase))
+            {
+                int value1 = Convert.ToInt32(this.Request.Form["input1"]);
+                int value2 = Convert.ToInt32(this.Request.Form["input2"]);
+                string operation = this.Request.Form["operation"];
+
+                ViewBag.input1 = value1;
+                ViewBag.input2 = value2;
+                ViewBag.operation = operation;
+                ViewBag.result = Calc(value1, value2, operation);
+
+            }
+
+            return View("Result");
+        }
+
+        [HttpPost, ActionName("Count2")]
+        public IActionResult Count2Post()
+        {
+            int value1 = Convert.ToInt32(this.Request.Form["input1"]);
+            int value2 = Convert.ToInt32(this.Request.Form["input2"]);
+            string operation = this.Request.Form["operation"];
+
+            ViewBag.input1 = value1;
+            ViewBag.input2 = value2;
+            ViewBag.operation = operation;
+            ViewBag.result = Calc(value1, value2, operation);
+
+            return View("Result");
+        }
+
+        [HttpPost]
+        public IActionResult Count3(int input1, int input2, string operation)
+        {
+            ViewBag.input1 = input1;
+            ViewBag.input2 = input2;
+            ViewBag.operation = operation;
+            ViewBag.result = Calc(input1, input2, operation);
+
+            return View("Result");
+        }
+
+
+        [HttpPost]
+        public IActionResult Count4(CalcModel calc)
+        {
+
+            ViewBag.input1 = calc.value1;
+            ViewBag.input2 = calc.value2;
+            ViewBag.operation = calc.operation;
+            ViewBag.result = calc.Calc();
+
+            return View("Result");
+        }
+    }
+}
---
+++ Web2/Web2/Models/CalcModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web2.Models
+{
+    public class CalcModel
+    {
+        public double value1 { get; set; }
+        public double value2 { get; set; }
+        public string operation { get; set; }
+
+        public CalcModel()
+        {
+
+        }
+
+        public CalcModel(double Value1, double Value2, string Operation)
+        {
+            value1 = Value1;
+            value2 = Value2;
+            operation = Operation;
+        }
+
+        public double Calc()
+        {
+            double result;
+
+            switch (operation)
+            {
+                case "+":
+                    result = value1 + value2;
+                    break;
+                case "-":
+                    result = value1 - value2;
+                    break;
+                case "*":
+                    result = value1 * value2;
+                    break;
+                case "/":
+                    result = value1 / value2;
+                    break;
+                default:
+                    result = 0;
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
