d1fd7724588eb0eb100cf23cd42eb5b60219a91a
nastyak0t <64741006+nastyak0t@users.noreply.github.com>
2020-05-20 13:06:26 +0700

    lab

---
+++ Web3/Web3/Controllers/QuizController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Lab3.Models;
+
+namespace Lab3.Controllers
+{
+    public class QuizController : Controller
+    {
+        static Random rnd = new Random();
+        static QuizModel quiz = new QuizModel();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Quiz()
+        {
+            ViewBag.numb1 = rnd.Next(10);
+            ViewBag.numb2 = rnd.Next(10);
+            return View();
+        }
+
+        public IActionResult QuizResult()
+        {
+            ViewBag.ListResult = quiz.listResult;
+            ViewBag.RightAnswersCount = quiz.rightAnswersCount;
+            ViewBag.AnswersCount = quiz.answersCount;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Next(string value1, string value2, string answer)
+        {
+            int Value1 = Convert.ToInt32(value1);
+            int Value2 = Convert.ToInt32(value2);
+            int Answer = Convert.ToInt32(answer);
+
+            quiz.listResult.Add(Value1 + " + " + Value2 + " = " + Answer);
+            quiz.answersCount++;
+
+            if (Value1 + Value2 == Answer)
+            {
+                quiz.rightAnswersCount++;
+            }            
+
+            ViewBag.numb1 = rnd.Next(10);
+            ViewBag.numb2 = rnd.Next(10);
+
+            return View("quiz");
+        }
+
+        [HttpPost]
+        public IActionResult Finish()
+        {
+            ViewBag.ListResult = quiz.listResult;
+            ViewBag.RightAnswersCount = quiz.rightAnswersCount;
+            ViewBag.AnswersCount = quiz.answersCount;
+            return View("quizResult");
+        }
+    }
+}
---
+++ Web3/Web3/Models/QuizModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab3.Models
+{
+    public class QuizModel
+    {
+        public int answer { get; set; } = -1;
+        public int rightAnswersCount { get; set; } = 0;
+        public int answersCount { get; set; } = 0;
+        public List<string> listResult { get; set; } = new List<string>();
+
+        public QuizModel(string result, int rightAnsw, int countAnsw)
+        {
+
+        }
+
+        public QuizModel()
+        {
+
+        }
+    }
+}
