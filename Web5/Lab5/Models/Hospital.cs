f6e89941b336c5fde4fd50c319ff4cd75babdfc7
nastyak0t <64741006+nastyak0t@users.noreply.github.com>
2020-06-07 17:17:42 +0700

    Web5

---
+++ Web5/Lab5/Models/Hospital.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab5.Models
+{
+    public class Hospital
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phones { get; set; }
+    }
+}
---
+++ Web5/Lab5/Models/Lab.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab5.Models
+{
+    public class Lab
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
